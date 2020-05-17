f9bfb42f9ec7440982e9b4ccb0928dbce3761626
褚鹏飞 <540788769@qq.com>
2020-05-18 06:13:52 +0800

    feat: add 小米的egg-xiaomi 的数据库创建完成

--- app/controller/admin/login.js
+++ app/controller/admin/login.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-11 19:13:16
- * @LastEditTime: 2020-05-16 20:11:50
+ * @LastEditTime: 2020-05-17 15:29:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /egg-xiaomi/app/controller/admin/login.js
@@ -19,6 +19,14 @@ class LoginController extends BaseController {
   async doLogin() {
     // 因为是继承base.js 所以直接调用success方法
     console.log(this.ctx.request.body);
+
+    // 获取用户传递过来的姓名
+    const username = this.ctx.request.body.username;
+    // 这里将md5 封装成了一个统一的方法
+    const password = await this.service.tools.md5func(this.ctx.request.body.password);
+    const code = this.ctx.request.body.verify;
+    // 这里跟着老师的版本走有点坑，之前改成的 verify 这里又使用回来了code
+    console.log(username, password, code);
   }
 }
 
--- app/service/tools.js
+++ app/service/tools.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2018-09-02 13:45:54
- * @LastEditTime: 2020-05-16 08:51:46
+ * @LastEditTime: 2020-05-17 10:57:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /egg-xiaomi/app/service/tools.js
@@ -10,6 +10,7 @@
 
 const svgCaptcha = require('svg-captcha');
 const Service = require('egg').Service;
+const md5 = require('md5');
 
 class ToolsService extends Service {
   // 这个 工具方法用于生成验证码
@@ -24,6 +25,10 @@ class ToolsService extends Service {
 
     this.ctx.session.code = captcha.text;
     return captcha;
+  }
+  // 封装MD5方法
+  async md5func(str) {
+    return md5(str);
   }
 }
 
